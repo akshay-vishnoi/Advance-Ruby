5122bf8f44d4b93cfcf65f7bd9366b205fcb3327
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-08-13 21:39:59 +0530

    now we can call after n bfore in ny sequence

--- e-10/e-10.rb
+++ e-10/e-10.rb
@@ -2,12 +2,12 @@
 module Filters
   def self.included(klass)
     klass.instance_eval do
-      @@args_before = []
+      @@args_before = nil
       def klass.before_filter(*args)
         class_meths = self.instance_methods - Object.instance_methods
         class_meths, args = simplify(class_meths, args)
-        @@args_before = args 
-        class_meths -= args
+        @@args_before ||= args 
+        class_meths -= (args + @@args_before)
         class_meths.each do |func_name|
           m = instance_method(func_name)
           define_method(func_name) do |*arguments, &block|
@@ -19,11 +19,11 @@ module Filters
         end
       end
       def klass.after_filter(*args)
-        class_meths_after = self.instance_methods - Object.instance_methods
+        class_meths = self.instance_methods - Object.instance_methods
         class_meths, args = simplify(class_meths, args)
-        class_meths_after -= args
-        class_meths_after -= @@args_before
-        class_meths_after.each do |func_name|
+        @@args_before ||= args
+        class_meths -= (args + @@args_before)
+        class_meths.each do |func_name|
           m = instance_method(func_name)
           define_method(func_name) do |*arguments, &block|
             m.bind(self).(*arguments, &block)
@@ -63,11 +63,10 @@ class Sample
   def main_function
     puts "in main_function"
   end
-  before_filter :foo, :bar, :only => [:main_function]
   after_filter :test_method, :only => [:main_function]
+  before_filter :foo, :bar, :only => [:main_function]
 end
 
 #main
 s = Sample.new
 s.main_function
-
