9c7e58aa05975f32e8bdd7e5469eda527ab33f98
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-08-25 18:08:59 +0530

    using instance_eval only

--- e-3/e-3.rb
+++ e-3/e-3.rb
@@ -3,12 +3,8 @@ puts "\nEnter name of the function"
 func_name = gets.chomp
 puts "\nEnter 1-line statement for function"
 func_statement = gets.chomp
+instance_eval "def #{func_name}
+  #{func_statement}
+end"
 puts "\nCalling method: #{func_name}()\n"
-    self.class_eval do
-define_method(func_name) do
-        eval(func_statement)
-end
-
-end
-
-eval "func_name"
+instance_eval func_name
